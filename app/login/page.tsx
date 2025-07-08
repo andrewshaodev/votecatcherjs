1bbf7438a018adabd1f73a9ad3f14700df231cf9
Andrew Shao <andrew@andrewshaodev.com>
2025-07-07 20:01:11 -0400

    add changes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,20 @@
 import { login, signup } from './actions'
+import styles from '@/app/page.module.css'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className={styles.main}>
+      <div className={styles.card}>
+        <h2>Login</h2>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input id="email" name="email" type="email" required />
+          <label htmlFor="password">Password:</label>
+          <input id="password" name="password" type="password" required />
+          <button formAction={login}>Log in</button>
+          <button formAction={signup}>Sign up</button>
+        </form>
+      </div>
+    </div>
   )
 }
